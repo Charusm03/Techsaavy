4a5ccef3dcff0474e4629c29edfe7e221f128ce5
Lingesh Jayakumar <146103425+Lingesh-coder@users.noreply.github.com>
2025-10-11 00:25:55 +0530

    Change import statement for FloatingNavDemo

--- src/app/components/TitleCard.tsx
+++ src/app/components/TitleCard.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { FloatingNavDemo } from './Navbar';
+import FloatingNavDemo from './Navbar';
 
 const TitleCard = () => {
   const [isMobile, setIsMobile] = useState(false);
